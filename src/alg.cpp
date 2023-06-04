e2bf9ad6da0f904a84e1a97919462334ad0684fc
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-06-04 20:52:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@
 #include  "tree.h"
 int factorial(int n) {
     if (n <= 1) return 1;
-    return fact(n - 1) * n;
+    return factorial(n - 1) * n;
 }
 std::vector<char> getPerm(const Tree& tree, int n) {
     if (tree.getSize() == 0 || n > factorial(tree.getSize()))
